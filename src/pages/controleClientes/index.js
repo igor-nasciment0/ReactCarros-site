e317d31a4e27b51f58a2850fcb87643d809a3130
igao <nascimentoigor1311@gmail.com>
2023-08-27 08:36:27 -0300

    barra lateral e cabecalho

--- src/pages/controleClientes/index.js
+++ src/pages/controleClientes/index.js
@@ -1,9 +1,16 @@
+import './index.scss';
+
 import BarraLateral from "../../components/barraLateral";
+import Cabecalho from "../../components/header";
 
 export default function ControleClientes() {
     return(
         <div className="pagina-clientes">
             <BarraLateral />
+
+            <main>
+                <Cabecalho />
+            </main>
         </div>
     )
 }
